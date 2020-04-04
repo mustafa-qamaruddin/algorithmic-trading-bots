7a43cbf38a32bf3d51f0bbe54810bd3facfee82c
Mustafa Qamar-ud-Din <mustafa@mQuBits.com>
2020-04-04 11:05:52 +0200

    Zipline Issue #2627

--- Eclipse Projects/Section 1/main.py
+++ Eclipse Projects/Section 1/main.py
@@ -2,9 +2,13 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 from strategies.run_zipline import run_strategy
+import zipline
+import os
 
 
 def main():
+    zipline_dir = os.path.dirname(zipline.__file__)
+    print("*** Zipline is installed @ {} ***".format(zipline_dir))
     print("*** PackPub - Hands-on Machine Learning for Algorithmic Trading Bots ***")
     print("*** SEC001/VID005: Build the Conventional Buy and Hold Strategy ***")
     perf = run_strategy("buy_and_hold")
